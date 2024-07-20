cc2e6b4edb674f68d28bf41a01dae9ffc5104855
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-20 22:10:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID", "7044783841"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI") # Don't fill it in, just fill it in vi .env warning!
 MUST_JOIN = getenv("MUST_JOIN", None)
